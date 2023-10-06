0f41187b51ee656b61eb9a8e215964d2ff1c649d
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-10-06 10:10:23 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+// <<<<<<<<<< CHAPTER NO: 26 TO 30 >>>>>>>>>>
+// ANSWER NO: 01
+// let input = +prompt("Enter A number in decimal..");
+// document.write(`Number: ${input}`);
+// document.write(`<br>Round off value of the number: ${Math.round(input)}`);
+// document.write(`<br>Floor value of the number: ${Math.floor(input)}`);
+// document.write(`<br>Ciel value of the number: ${Math.ceil(input)}`);
+
+// ANSWER NO: 02
+// let input = +prompt("Enter A number in decimal..");
+// document.write(`Number: ${input}`);
+// document.write(`<br>Round off value of the number: ${Math.round(input)}`);
+// document.write(`<br>Floor value of the number: ${Math.floor(input)}`);
+// document.write(`<br>Ciel value of the number: ${Math.ceil(input)}`);
+
+// ANSWER NO: 03
+// let input = +prompt("Enter a number..");
+// if(input < 0){
+//     let absoluteValue = -input;
+//     alert(`The absolute value of ${input} is ${absoluteValue}`);
+// }else{
+//     alert(`The absolute value of ${input} is ${input}`);
+// }
+
+
+
+
+// <<<<<<<<<< CHAPTER NO: 31 TO 34 >>>>>>>>>>
+
+// ANSWER NO: 01
+// let date = new Date();
+// document.write(date);
+
+// ANSWER NO: 02
+// let date = new Date();
+// let month = date.getMonth()
+// let arr = ["Januray","Febraury","March","April","May","June","July","August","September","October","November","December"];
+// alert("Current Month: " + arr[month]);
+
+// ANSWER NO: 03
+// let arr = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+// let date = new Date();
+// let day = date.getDay();
+// alert("Today is " + arr[day]);
+
+// ANSWER NO: 04
+// let arr = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// let date = new Date();
+// let day = date.getDay();
+// if (arr[day] == "Sun" || arr[day] == "Sat") {
+//   alert("It's a Funday..");
+// } else {
+//   alert("Today is: " + arr[day]);
+// }
+
+// ANSWER NO: 05
+// let day = new Date().getDate();
+// if (day <= 15) {
+//   alert("First fifteen days of the Month..");
+// } else {
+//   alert("Last fifteen days of the Month..");
+// }
+
+// ANSWER NO: 06
+// let date = new Date();
+// document.write("Current Date: " + date);
+
+// let milliSecond = date.getTime();
+// document.write(`<br>Elapsed millisecond since January 1, 1970: ${milliSecond}`);
+
+// milliSecond = date.getTime();
+// let result = milliSecond / 1000 / 60;
+// document.write(`<br>Elapsed millisecond since January 1, 1970: ${result}`);
+
+// ANSWER NO: 07
+// let date = new Date();
